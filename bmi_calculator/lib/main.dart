3f3ba54d5bd052b919c7495f0177192116944ae3
rrc_f2023_Dongok Yang <dyang2@academic.rrc.ca>
2023-12-07 12:50:58 -0600

    Bmi Calculater

---
+++ bmi_calculator/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bmi_calculator/input_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFF0A0E21),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+    );
+  }
+}
